644a18a2fb506b87bdd2dc16eae5c7785b2531c7
m0nika44 <monika.samyal.4@gmail.com>
2024-07-01 15:07:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.from("h1",{
+    left: 100,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5
+});
+gsap.from(".nav",{
+    bottom: 100,
+    opacity: 0,
+    duration: 1,
+    delay: 0.3
+});
